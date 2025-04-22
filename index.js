cea2e70e188d34f90d35295337704d20a8c838bd
DarkXz7 <miguelangelok9@hotmail.com>
2025-04-22 13:31:57 -0500

    hola

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Configuración de EJS como motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', './views'); // carpeta donde estarán tus plantillas EJS
+
+// Ruta para renderizar una plantilla
+app.get('/', (req, res) => {
+    const data = { title: 'Mi Proyecto en Node.js', message: '¡Hola, mundooo!' };
+    res.render('index', data); // Renderiza la plantilla "index.ejs" con los datos
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
